41554a99013867ce51b06dcfca07e8c061ef5152
Ganti Sai Sagar <sagarkr1109@gmail.com>
2023-09-21 19:33:59 +0530

    final commit of the class

--- ArrayListExamples.java
+++ ArrayListExamples.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+
 public class ArrayListExamples {
     public static void main(String[] args) {
+//        when we don't know how much sized array we need, we use ArrayList
+//        Syntax to
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(67);
+        arrayList.add(934);
+        arrayList.add(167);
+        arrayList.add(578);
+        arrayList.add(373);
+        arrayList.add(957);
+        arrayList.add(6);
+
+//        to remove element from nth index
+        System.out.println(arrayList.remove(3));
+//        to check if an element exists in an arrayList
+        System.out.println(arrayList.contains(578));
+//        to print the elements of an array
+        System.out.println(arrayList);
+//        to fetch element at nth index
+        for (int i = 0; i < 6 ; i++) {
+            System.out.println(arrayList.get(i));
+        }
 
     }
 }
