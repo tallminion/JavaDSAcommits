a26fc28a2b281be21a69f8689c26c607e08acf34
tall_minion <sagarkr1109@gmail.com>
2023-09-21 16:33:46 +0530

    commit trigger check

--- TwoDArraysBasics.java
+++ TwoDArraysBasics.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class TwoDArraysBasics {
     public static void main(String[] args) {
         System.out.println("hello World");
+        Scanner in = new Scanner(System.in);
+
     }
 }
