e47434c09f3d4d822796a02cfe5debc658ee2ff1
Ganti Sai Sagar <sagarkr1109@gmail.com>
2023-09-26 00:38:51 +0530

    commit Floor of Array

---
+++ BinarySearch/FloorArray.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class FloorArray {
+    public static void main(String[] args) {
+        int[] arr = {-8, -6, -4, 0, 2, 4, 7, 8, 10, 18, 22, 64, 78, 98};
+        int target = 63;
+        System.out.println(search(arr, target));
+    }
+
+    static int search(int[] arr, int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if(target<arr[mid]){
+                end = mid-1;
+            }else if(target>arr[mid]){
+                start = mid+1;
+            }else return mid;
+        }
+
+        return arr[end];
+
+    }
+}
