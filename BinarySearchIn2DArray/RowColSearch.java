a99e909d1862e70e70682f8ef14358ad0d3a48e3
Ganti Sai Sagar <sagarkr1109@gmail.com>
2023-09-27 01:12:44 +0530

    commit search in 2D array

---
+++ BinarySearchIn2DArray/RowColSearch.java
@@ -0,0 +1,30 @@
+package BinarySearchIn2DArray;
+
+import java.util.Arrays;
+
+public class RowColSearch {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+        int target = 35;
+        System.out.println(Arrays.toString(search(matrix, target)));
+    }
+
+    static int[] search(int[][] matrixArray, int target){
+        int row = 0;
+        int col = matrixArray.length-1;
+            while(row < matrixArray.length && col >= 0){
+                if (matrixArray[row][col]==target){
+                    return new int[] {row, col};
+                } else if (matrixArray[row][col]<target) {
+                    row++;
+                } else col--;
+            }
+            return new int[]{-1, -1};
+    }
+}
+
